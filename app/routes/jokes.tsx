aa9dacbb60662d14422ed7286f8eeac625ff47f5
cco-dev <cml_caner@hotmail.com>
2023-03-24 03:16:34 +0300

    db access

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,13 +1,30 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Outlet, Link } from "@remix-run/react";
+import { Joke } from "@prisma/client";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { Outlet, Link, useLoaderData } from "@remix-run/react";
 
 import stylesUrl from "~/styles/jokes.css";
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+type loaderData ={jokes :Array<Pick<Joke, "id" | "name">>}
+
+export const loader : LoaderFunction = async () => {
+    let jokes = await db.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    })
+    let data : loaderData = {jokes}
+    return data
+}
+
+
 export default function JokesRoute() {
+    let data = useLoaderData() as loaderData; 
+
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -30,9 +47,14 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+                {data.jokes.map(j => (
+                    <li key={j.id}>
+                    <Link to={j.id}>{j.name}</Link>
+                    </li>
+                ))
+        
+                }
+              
             </ul>
             <Link to="new" className="button">
               Add your own
@@ -46,3 +68,5 @@ export default function JokesRoute() {
     </div>
   );
 }
+
+
